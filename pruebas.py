9174a67ab92d254b5e833c59c91781cb0b4d5e13
Ismati5 <796919@unizar.es>
2023-04-10 20:45:27 +0200

    Añadida carpeta de mapas

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,7 @@
 from MapLib import Map2D
 import numpy as np
 
-map_file = "mapa1.txt"
+map_file = "maps/mapa1.txt"
 myMap = Map2D(map_file)
 myMap.fillCostMatrix(0, 0, 2, 0)
 myMap.planPath(0, 0, 2, 0)
